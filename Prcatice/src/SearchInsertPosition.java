f8657e81a1ff3f2572535be8f88c16465c506ef1
Pritpal Kaur <pritkaur97@gmail.com>
2020-05-25 15:38:44 -0400

    new question

---
+++ Prcatice/src/SearchInsertPosition.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SearchInsertPosition {
+
+	public static void main(String[] args) {
+		int[] nums= {1,3,5,6};
+		int target=5;
+		
+		searchInsert(nums,target);
+	}
+	private static void searchInsert(int[] nums, int target)
+	{
+		int i=0;
+		for(i=0; i<nums.length;i++) {
+			if(nums[i]>=target ) {
+				System.out.println(i);
+				break;
+			}
+			
+		}
+		
+	//return i;
+	
+}
+}
